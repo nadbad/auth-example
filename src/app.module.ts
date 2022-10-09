c1fcfd18a7dac4b056b3a4e8f44509bc1f7f2773
Nadeem <nadbadvlogs@gmail.com>
2022-10-09 12:09:58 +0200

    edited ports, added packages, etc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
+import { AbilityModule } from 'common/ability/ability.module';
 import { AuthModule } from './auth/auth.module';
 import { AtGuard } from './common/guards';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    PrismaModule,
+    AbilityModule,
+  ],
   providers: [
     {
       provide: APP_GUARD,
