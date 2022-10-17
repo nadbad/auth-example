114c4626fa9039fa13489aff0d80a576b771ffdf
Nadeem <nadbadvlogs@gmail.com>
2022-10-17 09:39:24 +0200

    added decorator

---
+++ src/common/decorators/api-tags-bearer.decorator.ts
@@ -0,0 +1,6 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+
+export function ApiTagsAndBearer(...tags: string[]) {
+  return applyDecorators(ApiBearerAuth(), ApiTags(...tags));
+}
